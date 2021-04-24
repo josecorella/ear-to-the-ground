ffa2d6ad5fdd7d696d9966e316147b5a75a9f4a6
Jose <joseeduardocorella@gmail.com>
2021-04-24 11:45:50 -0700

    Attempting promises -_-

--- src/map.js
+++ src/map.js
@@ -1,3 +1,6 @@
+var country_names = new Map();
+var svg = d3.select("svg");
+var g = svg.append("g");
 var projection = d3
   .geoMercator()
   .center([0, 0])
@@ -5,12 +8,12 @@ var projection = d3
   .rotate([0, 0])
   .translate([750, 800]);
 
-var country_names = new Map();
-var svg = d3.select("svg");
 var path = d3.geoPath().projection(projection);
-var g = svg.append("g");
-
-var promises = [d3.json("./data/world.json")];
+var promises = [
+  d3.json("./data/world.json", function (d) {
+    console.log(d);
+  }),
+];
 
 Promise.all(promises).then(ready);
 
@@ -22,7 +25,6 @@ function ready([world]) {
     .attr("d", path)
     .attr("fill", (d, i) => (i % 2 == 0 ? "rgb(30,215,96)" : "rgb(25, 20, 20)"))
     .style("stroke", "grey")
-    .attr("d", path)
     .append("title")
     .text(function (d, i) {
       return d.properties.name;
