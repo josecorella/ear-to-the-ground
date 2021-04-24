69ec93ddd7e04d0f1a36248dca8a73f4132ca846
Jose <joseeduardocorella@gmail.com>
2021-04-24 01:48:45 -0700

    Adding map

--- src/map.js
+++ src/map.js
@@ -1,23 +1,18 @@
-const w = 1000;
-const h = 800;
+var projection = d3.geoMercator().center([0, 0]).scale(150).rotate([0, 0]);
 
-var projection = d3.geoMercator().center([0, 5]).scale(150).rotate([-180, 0]);
-
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
+var svg = d3.select("svg");
 var path = d3.geoPath().projection(projection);
-
 var g = svg.append("g");
 
-// load and display the World
-d3.json("world-110m2.json").then(function (topology) {
+var promises = [d3.json("../data/world.json")];
+
+Promise.all(promises).then(ready);
+
+function ready([world]) {
+  console.log(world);
   g.selectAll("path")
-    .data(topojson.feature(topology, topology.objects.countries).features)
+    .data(topojson.feature(world, world.objects.countries).features)
     .enter()
     .append("path")
     .attr("d", path);
-});
+}
