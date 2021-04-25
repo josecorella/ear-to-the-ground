94210a04105647b6f39cbde9ecbe4966406f416f
Jose <joseeduardocorella@gmail.com>
2021-04-25 14:01:48 -0700

    Modifying csv files to remove weird first line

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-var country_names = new Map();
+var country_charts = new Map();
 var svg = d3.select("svg");
 var g = svg.append("g");
 var projection = d3
@@ -13,17 +13,20 @@ var path = d3.geoPath().projection(projection);
 var promises = [d3.json("./data/world.json")];
 
 function process_world_data([world]) {
-  // world.objects.countries.geometries.forEach((element) => {
-  //   if (element.properties.hasOwnProperty("available")) {
-  //     console.log("Spotify is available in: ", element.properties.name);
-  //   } else {
-  //     console.log("Spotify is not available in: ", element.properties.name);
-  //   }
-  // });
+  world.objects.countries.geometries.forEach((element) => {
+    if (element.properties.hasOwnProperty("available")) {
+      if (element.properties.hasOwnProperty("charts")) {
+        country_charts.set(element.properties.name, element.properties.charts);
+      } else {
+        country_charts.set(element.properties.name, "No Data Found");
+      }
+    }
+  });
   return [world];
 }
 
 function ready([world]) {
+  console.log(country_charts);
   g.selectAll("path")
     .data(topojson.feature(world, world.objects.countries).features)
     .enter()
@@ -36,7 +39,7 @@ function ready([world]) {
     )
     .style("stroke", "grey")
     .append("title")
-    .text(function (d, i) {
+    .text(function (d) {
       return d.properties.name;
     });
 }
