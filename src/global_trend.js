0a0f6e9db12d05ffbe806a71f70180965a7e4391
Jose <joseeduardocorella@gmail.com>
2021-05-10 13:55:23 -0700

    Fixed Line Chart

--- src/global_trend.js
+++ src/global_trend.js
@@ -1,4 +1,4 @@
-var margin = { top: 20, right: 20, bottom: 50, left: 70 },
+var margin = { top: 20, right: 20, bottom: 50, left: 100 },
   width = 1000 - margin.left - margin.right,
   height = 700 - margin.top - margin.bottom;
 
@@ -6,6 +6,7 @@ var svg = d3
   .select("#line-graph")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
+  .attr("transform", "translate(" + 250 + "," + margin.top + ")")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -58,17 +59,21 @@ d3.csv("./data/april2021_global.csv").then(function (data) {
     .attr("transform", "translate(0," + height + ")")
     .attr("stroke", "white")
     .call(d3.axisBottom(xAxis))
-    .style("color", "white");
+    .style("color", "white")
+    .style("font-size", "14px");
 
   svg
     .append("g")
     .attr("stroke", "white")
     .call(d3.axisLeft(yAxis))
+    .style("font-size", "14px")
     .style("color", "white")
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 6)
     .attr("dy", ".71em")
+    .style("font-size", "14px")
     .style("text-anchor", "end")
+    .attr("fill", "white")
     .text("Number of Streams");
 });
