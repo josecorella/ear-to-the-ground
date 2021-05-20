10e3ef8b40d800872c7a04471f5dafdc34958553
Jose <joseeduardocorella@gmail.com>
2021-05-20 15:06:08 -0700

    Finished TreeMap

--- src/breakdown.js
+++ src/breakdown.js
@@ -6,20 +6,12 @@ var hoverColor = "#eec42d";
 var staticColor = "rgb(30,215,96)";
 
 var canvas = d3
-  .select("#dua_breakdown")
-  .attr("width", b_width + b_margin.left + b_margin.right)
+  .select("#breakdown")
+  .attr("width", 1000)
   .attr("height", b_height + b_margin.top + b_margin.bottom)
-  .attr("transform", "translate(" + -75 + "," + b_margin.top + ")")
+  .attr("transform", "translate(" + 150 + "," + b_margin.top + ")")
   .append("g")
-  .attr("transform", "translate(" + b_margin.left + "," + b_margin.top + ")");
-
-var canvas2 = d3
-  .select("#bieber_breakdown")
-  .attr("width", b_width + b_margin.left + b_margin.right)
-  .attr("height", b_height + b_margin.top + b_margin.bottom)
-  .attr("transform", "translate(" + 675 + "," + -678 + ")")
-  .append("g")
-  .attr("transform", "translate(" + b_margin.left + "," + b_margin.top + ")");
+  .attr("transform", "translate(" + 10 + "," + b_margin.top + ")");
 
 var tooltip = d3
   .select("body")
@@ -36,7 +28,17 @@ var tooltip = d3
 var parseTime = d3.timeParse("%Y%m%d");
 var formatTime = d3.timeFormat("%B %e");
 
-d3.csv("./data/dua_breakdown.csv").then(function (data) {
+var color = d3
+  .scaleOrdinal()
+  .domain([
+    "Levitating (feat. DaBaby) by Dua Lipa",
+    "Peaches (feat. Daniel Caesar & Giveon) by Justin Bieber",
+    "Leave The Door Open by Bruno Mars Anderson .Paak Silk Sonic",
+  ])
+  .range(["#20d4d4", "#1ed760 ", "#1e48d4"]);
+var opacity = d3.scaleLinear().domain([5, 10]).range([0.8, 1]);
+
+d3.csv("./data/breakdown.csv").then(function (data) {
   var root = d3
     .stratify()
     .id(function (d) {
@@ -46,10 +48,12 @@ d3.csv("./data/dua_breakdown.csv").then(function (data) {
       return d.parent;
     })(data);
   root.sum(function (d) {
-    return +d.streams;
+    if (d.song !== "") {
+      return +d.name;
+    }
   });
 
-  d3.treemap().size([b_width, b_height]).padding(4)(root);
+  d3.treemap().size([b_width, b_height]).padding(3)(root);
 
   canvas
     .selectAll("rect")
@@ -69,17 +73,23 @@ d3.csv("./data/dua_breakdown.csv").then(function (data) {
       return d.y1 - d.y0;
     })
     .style("stroke", "white")
-    .style("fill", "rgb(30,215,96)")
+    .style("fill", function (d) {
+      return color(d.data.song);
+    })
+    .style("opacity", function (d) {
+      console.log(d.value / 1000000);
+      return opacity(d.value / 1000000);
+    })
     .on("mouseover", function (event, i) {
       nfObject = new Intl.NumberFormat("en-US");
       tooltip
         .html(
           `<div>Day: ${formatTime(
-            parseTime(i.id)
+            parseTime(i.data.parent)
           )}</div><div>Streams: ${nfObject.format(i.value)}</div>`
         )
         .style("visibility", "visible");
-      d3.select(this).attr("fill", hoverColor);
+      d3.select(this).transition().attr("fill", hoverColor);
     })
     .on("mousemove", function (event) {
       tooltip
@@ -90,113 +100,29 @@ d3.csv("./data/dua_breakdown.csv").then(function (data) {
       tooltip.html(``).style("visibility", "hidden");
       d3.select(this).transition().attr("fill", staticColor);
     });
-
   canvas
     .selectAll("text")
     .data(root.leaves())
     .enter()
     .append("text")
     .attr("x", function (d) {
-      return d.x0 + 10;
+      return d.x0 + 5;
     })
     .attr("y", function (d) {
-      return d.y0 + 20;
+      return d.y0 + 10;
     })
     .text(function (d) {
-      return formatTime(parseTime(d.data.name));
+      return formatTime(parseTime(d.data.parent));
     })
-    .attr("font-size", "12px")
+    .attr("font-size", "6px")
     .attr("fill", "black");
-
   canvas
-    .append("text")
-    .attr("font-family", "Arial, Helvetica, sans-serif")
-    .attr("transform", "translate(325, 650)")
-    .style("text-anchor", "middle")
-    .style("font-size", "20px")
-    .attr("stroke", "white")
-    .attr("fill", "white")
-    .text("Levitating by Dua Lipa");
-});
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", "translate(700,20)")
+    .attr("fill", "white");
 
-d3.csv("./data/bruno_breakdown.csv").then(function (data) {
-  var root = d3
-    .stratify()
-    .id(function (d) {
-      return d.name;
-    })
-    .parentId(function (d) {
-      return d.parent;
-    })(data);
-  root.sum(function (d) {
-    return +d.streams;
-  });
+  var legend = d3.legendColor().title("Songs").titleWidth(100).scale(color);
 
-  d3.treemap().size([b_width, b_height]).padding(4)(root);
-
-  canvas2
-    .selectAll("rect")
-    .data(root.leaves())
-    .enter()
-    .append("rect")
-    .attr("x", function (d) {
-      return d.x0;
-    })
-    .attr("y", function (d) {
-      return d.y0;
-    })
-    .attr("width", function (d) {
-      return d.x1 - d.x0;
-    })
-    .attr("height", function (d) {
-      return d.y1 - d.y0;
-    })
-    .style("stroke", "white")
-    .style("fill", "rgb(30,215,96)")
-    .on("mouseover", function (event, i) {
-      nfObject = new Intl.NumberFormat("en-US");
-      tooltip
-        .html(
-          `<div>Day: ${formatTime(
-            parseTime(i.id)
-          )}</div><div>Streams: ${nfObject.format(i.value)}</div>`
-        )
-        .style("visibility", "visible");
-      d3.select(this).attr("fill", hoverColor);
-    })
-    .on("mousemove", function (event) {
-      tooltip
-        .style("top", event.pageY - 10 + "px")
-        .style("left", event.pageX + 10 + "px");
-    })
-    .on("mouseout", function (event, i) {
-      tooltip.html(``).style("visibility", "hidden");
-      d3.select(this).transition().attr("fill", staticColor);
-    });
-  canvas2
-    .selectAll("text")
-    .data(root.leaves())
-    .enter()
-    .append("text")
-    .attr("x", function (d) {
-      return d.x0 + 10;
-    })
-    .attr("y", function (d) {
-      return d.y0 + 20;
-    })
-    .text(function (d) {
-      return formatTime(parseTime(d.data.name));
-    })
-    .attr("font-size", "12px")
-    .attr("fill", "black");
-
-  canvas2
-    .append("text")
-    .attr("font-family", "Arial, Helvetica, sans-serif")
-    .attr("transform", "translate(330, 650)")
-    .style("text-anchor", "middle")
-    .style("font-size", "20px")
-    .attr("stroke", "white")
-    .attr("fill", "white")
-    .text("Leave The Door Open by Bruno Mars");
+  canvas.select(".legend").call(legend);
 });
